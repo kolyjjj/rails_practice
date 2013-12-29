da18606bbfb10dc8a9eb8aefbd55b312a3738a93
kolyjjj <kolyjjj@foxmail.com>
2013-12-29 15:45:35 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meidusa::Application.config.secret_key_base = 'bfc9888654d8d3ec6d49790a2fa63e2a897d8be2a985eafef490637d42707f781466a71cab1b492194c72578a683929346f1434a060062104efddf1648a455ce'
