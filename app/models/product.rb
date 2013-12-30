c1a0f73d398bb0a2eadb4125859f706ae936db98
kolyjjj <kolyjjj@foxmail.com>
2013-12-30 22:26:08 +0800

    add simple validation for production creation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ActiveRecord::Base
+  validate :title, presence: true
+  validate :description, presence: true, length: {minimum: 10}
+  validate :price, presence: true
+  validate :image_url, presence: true
+  validate :tags, presence: true
 end
