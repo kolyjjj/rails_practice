4abb5ad9ac7f865247bbccf97ed4bd112911b789
kolyjjj <kolyjjj@foxmail.com>
2014-01-03 09:20:20 +0800

    add customer message for errors

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ActiveRecord::Base
-  validates :title, presence: true
-  validates :description, presence: true, length: {minimum: 10}
-  validates :price, presence: true
+  validates :title, presence: {message: '请输入标题'}
+  validates :description, presence:{message: '请输入描述'}, length: {minimum: 10, message: '长度不小于10'}
+  validates :price, presence: {message: '请输入价格'}
   #validates :image_url, presence: true
   validates_with EmailValidator
-  validates :tags, presence: true
+  validates :tags, presence: {message: '请输入标签'}
 end
