05dcb5e9fdce04688c694de5e886433de1a9fa83
kolyjjj <kolyjjj@foxmail.com>
2014-01-03 09:20:44 +0800

    wording of the error message

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   validates :title, presence: {message: '请输入标题'}
-  validates :description, presence:{message: '请输入描述'}, length: {minimum: 10, message: '长度不小于10'}
+  validates :description, presence:{message: '请输入描述'}, length: {minimum: 10, message: '描述长度不小于10'}
   validates :price, presence: {message: '请输入价格'}
   #validates :image_url, presence: true
   validates_with EmailValidator
