63f6c943b3215855f65bcfcdb27014d5a5bb9965
kolyjjj <kolyjjj@foxmail.com>
2014-01-03 14:43:17 +0800

    add error messages to locale

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ActiveRecord::Base
-  validates :title, presence: {message: '请输入标题'}
+  validates :title, presence: true
   validates :description, presence:{message: '请输入描述'}, length: {minimum: 10, message: '描述长度不小于10'}
   validates :price, presence: {message: '请输入价格'}
-  #validates :image_url, presence: true
-  validates_with EmailValidator
+  validates :image_url, presence: true
+  #validates_with EmailValidator
   validates :tags, presence: {message: '请输入标签'}
 end
