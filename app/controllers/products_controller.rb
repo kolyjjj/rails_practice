8de3cb5c939d581dd9653e6985e4478b1f578dd5
kolyjjj <kolyjjj@foxmail.com>
2014-01-14 21:32:20 +0800

    add condition for create new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,9 +13,23 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  #def upload
+  #  uploaded_io = params[:product][:image_url]
+  #  File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+  #    file.write(uploaded_io.read)
+  #  end
+  #end
+
   def update
     @product = Product.find(params[:id])
+    logger.debug "the product to be updated is: -----#{@product.inspect}"
+    logger.debug "the new product is: -----#{param[:product]  .inspect}"
+
+    #product = params[:product]
     if @product.update(post_params)
+    #if @product.update({:title => product[:title], :description => product[:description],
+    #                        :price => product[:price], :tags => product[:tags],
+    #                        :image_url => product[:image_url].original_filename})
       redirect_to products_path
     else
       render :edit
@@ -23,11 +37,13 @@ class ProductsController < ApplicationController
   end
 
   def create
-    logger.debug "the product object is: -----#{params[:product].inspect}"
+    logger.debug "the product to be created is: -----#{params[:product].inspect}"
     product = params[:product]
+    pic = product[:image_url].original_filename if !product[:image_url].nil?
+
     @product = Product.new({:title => product[:title], :description => product[:description],
                             :price => product[:price], :tags => product[:tags],
-                            :image_url => product[:image_url].original_filename})
+                            :image_url => pic})
     if @product.save
       savePicture(product)
       redirect_to action: 'index'
@@ -52,7 +68,8 @@ class ProductsController < ApplicationController
     end
   end
 
-  def post_params
+  def product_params
+    logger.debug "parmas.require is: #{params.require(:product)}"
     params.require(:product).permit(:title, :description, :tags, :price, :image_url)
   end
 end
